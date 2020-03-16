8ec493e7db2f42950dd93167fc6123f70e2daa60
Emma McMillan <emma.mcmillan@microsoft.com>
2020-03-16 15:03:48 -0700

    Add a back Typscript 2 tests

--- test/types/index.d.ts
+++ test/types/index.d.ts
@@ -1,3 +1,7 @@
+// TypeScript Version: 2.6
+// TypeScript Version: 2.7
+// TypeScript Version: 2.8
+// TypeScript Version: 2.9
 // TypeScript Version: 3.0
 // TypeScript Version: 3.1
 // TypeScript Version: 3.2
