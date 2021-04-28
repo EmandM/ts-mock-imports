90fc0782d2fb7b092ae151da06123d3e9f2ba13b
EmandM <loopylemon8@gmail.com>
2021-04-28 12:47:20 -0700

    Update package and test versions

--- test/types/index.d.ts
+++ test/types/index.d.ts
@@ -1,7 +1,3 @@
-// TypeScript Version: 2.6
-// TypeScript Version: 2.7
-// TypeScript Version: 2.8
-// TypeScript Version: 2.9
 // TypeScript Version: 3.0
 // TypeScript Version: 3.1
 // TypeScript Version: 3.2
